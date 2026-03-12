8b0558ec226a3e45399ec88257d668b14be71e34
MartinRojas <rojas.martin25@yahoo.com>
2026-03-11 18:42:46 -0700

    feat(utils): add weatherstack module to fetch current weather by coordinates

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-import request from "request";
 import { geocode } from "./utils/geocode.js";
+import { weatherstack } from "./utils/forecast.js";
 
 // Callback pattern
 
-geocode(`pfluegerville`, (error, data) => {
+// geocode(`pfluegerville`, (error, data) => {
+//    console.log(`Error: `, error);
+//    console.log("Data: ", data);
+// });
+
+weatherstack(30.439, -97.62, (error, data) => {
    console.log(`Error: `, error);
-   console.log("Data: ", data);
+   console.log(`Data: `, data);
 });
---
+++ utils/forecast.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+import request from "request";
+
+dotenv.config({ quiet: true });
+
+const weatherstack = (latitude, longitude, callback) => {
+   const url = `https://api.weatherstack.com/current?access_key=${process.env.weatherstack_access_key}&query=${latitude},${longitude}`;
+
+   request({ url: url, json: true }, (error, response) => {
+      if (error) {
+         callback(`Unable to connect to location serivces`, undefined);
+      } else if (response.body.success === false) {
+         callback(`Unable to find location`, undefined);
+      } else {
+         callback(
+            undefined,
+            `The current weather is ${response.body.current.temperature} degrees out. There is
+                    ${response.body.current.precip}% chance of rain`,
+         );
+      }
+   });
+};
+
+export { weatherstack };
